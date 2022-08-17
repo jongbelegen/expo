2fb0353d72da4e05525b41174960641bbc3c16c3
lukmccall <kosmatylukasz@gmail.com>
2022-08-17 13:49:39 +0200

    [menu][iOS] Prebuild `react-native-gesture-handler`

---
+++ packages/expo-dev-menu/vendored/react-native-gesture-handler/RNGestureHandler.podspec
@@ -0,0 +1,12 @@
+Pod::Spec.new do |s|
+    s.name         = "DevMenuRNGestureHandler"
+    s.version      = "2.1.2"
+    s.homepage     = "https://github.com/software-mansion/react-native-gesture-handler"
+    s.license      = "MIT"
+    s.platform       = :ios, '12.0'
+    s.source       = { :git => "https://github.com/software-mansion/react-native-gesture-handler", :tag => "2.1.2" }
+    s.source_files = "ios/**/*.{h,m}"
+  
+    s.dependency "React-Core"
+  
+end
--- tools/src/commands/Vendor.ts
+++ tools/src/commands/Vendor.ts
@@ -298,10 +298,32 @@ function getGestureHandlerPipe() {
       new CopyFiles({
         filePattern: 'ios/**/*.@(h|m)',
         to: destination,
+      }),
+      new AddFile({
+        destination: `${destination}/RNGestureHandler.podspec`,
+        content: `Pod::Spec.new do |s|
+  s.name         = "DevMenuRNGestureHandler"
+  s.version      = "2.1.2"
+  s.homepage     = "https://github.com/software-mansion/react-native-gesture-handler"
+  s.license      = "MIT"
+  s.platform       = :ios, '12.0'
+  s.source       = { :git => "https://github.com/software-mansion/react-native-gesture-handler", :tag => "2.1.2" }
+  s.source_files = "ios/**/*.{h,m}"
+
+  s.dependency "React-Core"
+
+end
+`
       })
   );
 
-  return { transformations };
+  return {
+    transformations,
+    prebuild: {
+      podspecPath: `${destination}/RNGestureHandler.podspec`,
+      output: destination,
+    },
+  };
 }
 
 function getSafeAreaConfig() {
